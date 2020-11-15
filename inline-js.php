17894624250bbfc42b589ed51c06eed978124322
Reuven Karasik <rubik@karasik.org>
2020-11-16 00:34:49 +0200

    Saving the user's choice in localStorage

--- inline-js.php
+++ inline-js.php
@@ -8,12 +8,18 @@
   if (!window._disableIvrita) {
     window._ivrita = new Ivrita();
 
+    var mode;
+    if (mode = window.localStorage.getItem('ivrita-mode')) {
+      window._ivrita.setMode(Ivrita[mode]);
+    }
+
     Array.prototype.forEach.call(document.querySelectorAll('.ivrita-button'), function(el) {
       var btnMode = el.dataset.ivritaMode;
       if (btnMode && Ivrita.GENDERS.includes(Ivrita[btnMode])) {
         el.addEventListener('click', function(e) {
           e.preventDefault();
           window._ivrita.setMode(Ivrita[btnMode]);
+          window.localStorage.setItem('ivrita-mode', btnMode);
         })
       }
     });
