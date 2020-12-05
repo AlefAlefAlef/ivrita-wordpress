c1a9215aa05321ae6294bdeb73b369559ffdfddc
Reuven Karasik <rubik@karasik.org>
2020-12-05 13:55:48 +0200

    Improvements to translations, fonts and switch

--- template-switch.php
+++ template-switch.php
@@ -12,5 +12,5 @@ if ( 'right' === $position ) {
   <a href="#" class="ivrita-mode-changer ivrita-button ivrita-button-<?php echo $menu_style; ?>" data-ivrita-mode="MALE" title="<?php echo esc_attr( $male_label ); ?>">♂</a>
   <a href="#" class="ivrita-mode-changer ivrita-button ivrita-button-<?php echo $menu_style; ?>" data-ivrita-mode="FEMALE" title="<?php echo esc_attr( $female_label ); ?>">♀</a>
   <a href="#" class="ivrita-mode-changer ivrita-button ivrita-button-<?php echo $menu_style; ?> ivrita-active" data-ivrita-mode="NEUTRAL" title="<?php echo esc_attr( $neutral_label ); ?>">⚥</a>
-  <a href="https://alefalefalef.co.il/ivrita/" class="ivrita-info-link" title="<?php echo esc_attr_e( 'About the Ivrita project', 'ivrita' ); ?>">ⓘ</a>
+  <a href="https://alefalefalef.co.il/ivrita/" class="ivrita-info-link" title="<?php echo esc_attr_e( 'About the Ivrita project', 'ivrita' ); ?>" target="_blank">ⓘ</a>
 </div>
